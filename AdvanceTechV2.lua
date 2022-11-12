fa0047aca63f5285a5736bd6f6f30a23bf799342
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2022-11-12 03:54:36 -0500

    Create AdvanceTechV2.lua

--- AdvanceTechV2.lua
+++ AdvanceTechV2.lua
@@ -1057,7 +1057,6 @@ _G.FullBrightExecuted = true
 _G.FullBrightEnabled = not _G.FullBrightEnabled
 end)
 
-
 tab2:Button("Esp 2", function()
 local function API_Check()
     if Drawing == nil then
@@ -1253,6 +1252,7 @@ elseif Errored and not Success then
     TestService:Message("The ESP script has errored, please notify Exunys with the following information :")
     warn(Errored)
     print("!! IF THE ERROR IS A FALSE POSITIVE (says that a player cannot be found) THEN DO NOT BOTHER !!")
+end
 end)
 
 tab2:Button("FullBright 2", function()
